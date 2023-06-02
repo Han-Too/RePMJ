35470ea15ccd92f98c47746eb3743f5813dd95f6
Han <68423335+Han-Too@users.noreply.github.com>
2023-06-02 22:17:25 +0700

    ubah judul edit admin

--- resources/views/adminpage/pesananpages/editpesanan.blade.php
+++ resources/views/adminpage/pesananpages/editpesanan.blade.php
@@ -29,7 +29,7 @@
                         <!--begin::Header-->
                         <div class="card-header border-0 pt-5">
                             <h3 class="card-title align-items-start flex-column">
-                                <span class="card-label fw-bold fs-3 mb-1">Detail Edit Pesanan</span>
+                                <span class="card-label fw-bold fs-3 mb-1">Ubah Data Pesanan</span>
                                 {{-- <span class="text-muted mt-1 fw-semibold fs-7">Tambah atau Edit Data Karyawan</span> --}}
                             </h3>
 
